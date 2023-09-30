c83349c7ba924e71142a2da0e86822eff47918ba
chuklin <inkarttatoo@gmail.com>
2023-09-30 09:42:32 +0500

    fix avg_salary

--- src/hh_api.py
+++ src/hh_api.py
@@ -1,6 +1,6 @@
-import requests
 import json
 import time
+import requests
 
 
 class HeadHunter:
@@ -31,13 +31,16 @@ class HeadHunter:
 
 
 class Vacancy:
-    __data_path = 'data/vacancies.json'
+    """Собирает с сайта HeadHunter вакансии по номеру айди работодателя"""
+
+    __file_path = 'data/vacancies.json'
 
     def __init__(self, id_employer):
         self.id_employer = id_employer
-        self.get_vacancy = self.get_vacancies()
+        self.get_vacancy = self.get_vacancy()
 
-    def get_vacancies(self):
+    def get_vacancy(self):
+        """Возвращает вакансии по номеру айди работодателя"""
         vacancies_data = []
 
         for page in range(0, 10):
@@ -46,42 +49,44 @@ class Vacancy:
                 "page": page,
                 'per_page': 20,
             }
-            data = requests.get('https://api.hh.ru/vacancies', params=params).json()
+            data = requests.get("https://api.hh.ru/vacancies", params=params).json()
             vacancies_data.extend(data.get('items'))
             time.sleep(0.22)
 
-        with open(self.__data_path, "w", encoding="utf-8") as f:
+        with open(self.__file_path, "w", encoding="utf-8") as f:
             json.dump(vacancies_data, f, ensure_ascii=False, indent=4)
+
         return vacancies_data
 
     def add_vacancies_to_list(self):
-        vacancies_list = []
+        """Записывает найденные вакансии с нужными ключами в список словарей"""
+        list_vacancy = []
         vacancies = self.get_vacancy
 
-        for data in range(len(vacancies)):
-            salary_from = 0 if (vacancies[data]['salary'] is None or vacancies[data]['salary']['from'] == 0 or
-                                vacancies[data]['salary']['from'] is None) else vacancies[data]['salary']['from'],
-            salary_to = 0 if (vacancies[data]['salary'] is None or vacancies[data]['salary']['to'] == 0 or
-                              vacancies[data]['salary']['to'] is None) else vacancies[data]['salary']['to']
-
+        for i in range(len(vacancies)):
+            salary_from = 0 if (vacancies[i]['salary'] is None or vacancies[i]['salary']['from'] == 0 or
+                                vacancies[i]['salary']['from'] is None) else vacancies[i]['salary'][
+                'from']
+            salary_to = 0 if (vacancies[i]['salary'] is None or vacancies[i]['salary']['to'] == 0 or
+                              vacancies[i]['salary']['to'] is None) else vacancies[i]['salary']['to']
             vacancy_data = {
-                'id_vacancy': vacancies[data].get('id'),
-                'name_vacancy': vacancies[data].get('name'),
-                'id_employer': 0 if vacancies[data]['employer']['id'] is None else vacancies[data]['employer'][
+                'id_vacancy': vacancies[i].get('id'),
+                'name_vacancy': vacancies[i].get('name'),
+                'id_employer': 0 if vacancies[i]['employer']['id'] is None else vacancies[i]['employer'][
                     'id'],
-                'name_employer': "Не указано" if vacancies[data]['employer']['name'] is None
-                else self.get_vacancy[data]['employer']['name'],
-                'city': "Не указано" if vacancies[data]['area']['name'] is None else vacancies[data]['area'][
+                'name_employer': "Не указано" if vacancies[i]['employer']['name'] is None else
+                self.get_vacancy[i]['employer']['name'],
+                'city': "Не указано" if vacancies[i]['area']['name'] is None else vacancies[i]['area'][
                     'name'],
                 'salary_from': salary_from,
                 'salary_to': salary_to,
-                'salary_avg': 0,
-                # (salary_from if salary_to == 0 else (salary_from + salary_to) / 2) or (
-                # salary_to if salary_from == 0 else (salary_to + salary_to) / 2),
-                'experience': vacancies[data]['experience'].get('name'),
-                'url': vacancies[data].get('alternate_url'),
-                "requirement": "Не указано" if vacancies[data]['snippet']['requirement'] else
-                vacancies[data]['snippet']['requirement'],
+                'salary_avg': (salary_from if salary_to == 0 else (salary_from + salary_to) / 2) or (
+                    salary_to if salary_from == 0 else (salary_to + salary_to) / 2),
+                'experience': vacancies[i]['experience'].get('name'),
+                'url': vacancies[i].get('alternate_url'),
+                "requirement": "Не указано" if vacancies[i]['snippet']['requirement'] else
+                vacancies[i]['snippet']['requirement'],
             }
-            vacancies_list.append(vacancy_data)
-        return vacancies_list
+
+            list_vacancy.append(vacancy_data)
+        return list_vacancy
